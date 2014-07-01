a43205b25f5179a16d817db9fc0938735447acc1
"Clifford Beshers <beshers@seereason.com>
2014-07-01 03:17:25 +0000

    make dpkg-buildpackage work by stealing Setup.hs from ghcjs-dom-hello

--- happstack-ghcjs-server/Setup.hs
+++ happstack-ghcjs-server/Setup.hs
@@ -1,6 +1,19 @@
 #!/usr/bin/runhaskell 
 module Main where
-import Distribution.Simple
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, UserHooks(postBuild))
+import Distribution.Simple.LocalBuildInfo (buildDir)
+import System.FilePath ((</>), (<.>))
+import System.IO (appendFile)
+import System.Process (readProcess)
+
+packageName :: FilePath
+packageName = "happstack-ghcjs-client"
+
 main :: IO ()
-main = defaultMain
+main = defaultMainWithHooks $ simpleUserHooks
+         { postBuild = \ _ _ _ lbi -> let prefix = buildDir lbi </> packageName </> packageName <.> "jsexe" 
+                                          binpath = "usr/bin" </> packageName <.> "jsexe"
+                                      in
+                                        readProcess "find" [prefix, "-type", "f"] "" >>=
+                                        writeFile ("debian" </> packageName <.> "install") . unlines . map (++ " " ++ binpath) . lines }
 
