234eafcd453eb670f54c6c9430a2126d5e44a0be
"Clifford Beshers <beshers@seereason.com>
2014-07-03 00:44:12 +0000

    Added lots of logging, but screwed up the routing.  Happstack routing is too much like assembler.

--- server/Server.hs
+++ server/Server.hs
@@ -1,34 +1,85 @@
 module Main where
 
 import Control.Monad as Monad (msum)
+import Control.Monad.Trans as Trans (liftIO)
 import Data.Time.Format (FormatTime(..))
-
-import Happstack.Server
-import System.IO (hPutStrLn, stderr, stdout)
-import System.Log.Handler.Simple (fileHandler, streamHandler)
-import System.Log.Logger -- (Priority(DEBUG), debugM, logM, rootLoggerName, setHandlers, setLevel, updateGlobalLogger)
+import Happstack.Server (asContentType, Browsing(EnableBrowsing), Conf(logAccess, port), dir, LogAccess, nullConf, Response, serveDirectory, serveFile, ServerPartT, simpleHTTP)
 import Happstack.Server.Internal.LogFormat (formatRequestCombined)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath as FilePath ((<.>), (</>))
+import System.IO (stdout)
+import System.Log.Handler.Simple (fileHandler, streamHandler)
+import System.Log.Logger (debugM, logM, Priority(ALERT, DEBUG, INFO), rootLoggerName, setHandlers, setLevel, updateGlobalLogger)
 --import StaticResources
+import EmbedGHCJS
+
+data LogMode
+    = Production
+    | Development
+    deriving (Read, Show, Eq, Ord, Enum, Bounded)
 
 logMAccess' :: FormatTime t => LogAccess t
 logMAccess' host user time requestLine responseCode size referer userAgent =
     logM "SimpleServer" DEBUG $ formatRequestCombined host user time requestLine responseCode size referer "" -- userAgent
 
+application :: GHCJSPackageName
+application = "ghcjs-dom-hello"
 
 main :: IO ()
 main = do
   -- stdoutLog <- streamHandler stdout DEBUG
-  updateGlobalLogger "SimpleServer" (setLevel DEBUG)
-  debugM "SimpleServer" "Global logger started at level DEBUG."
+  updateGlobalLogger application (setLevel DEBUG)
+  debugM application "Global logger started at level DEBUG."
   let p = 8010
   debugM "SimpleServer" $ "Starting server on port " ++ show p
-  simpleHTTP (nullConf { port = p, logAccess = Just logMAccess' }) $ handlers
+  let logDir = "_local"
+  setupLogger logDir Development
+  logM "Main.hs" ALERT $ "Setting logging directory to be top directory (" ++ logDir ++ ") "
+  simpleHTTP (nullConf { port = p, logAccess = Just logMAccess' }) $ handlers Development
   
-handlers :: ServerPartT IO Response
-handlers = 
+handlers :: LogMode -> ServerPartT IO Response
+handlers mode = do
+  let package = "happstack-ghcjs-client"
+  -- liftIO $ logM "Server.hs/handlers" ALERT $ "Serving " ++ package
   msum $ -- staticResourceParts "/static" ++
        [ dir "/manifest.appcache" $ serveFile (asContentType "text/cache-manifest") "manifest.appcache"
        , dir "/favicon.ico" $ serveFile (asContentType "image/x-icon") "favicon.ico"
-       , dir "/" $ serveFile (asContentType "text/html") "dist/build/happstack-ghcjs-server/happstack-ghcjs-server.jsexe/index.html"
-       , serveDirectory EnableBrowsing [] "."
+       , ghcjsHandler mode package
+       , serveDirectory EnableBrowsing [] "/home/beshers/alldarcs/src.seereason.com"
        ]
+
+ghcjsHandler :: LogMode -> GHCJSPackageName -> ServerPartT IO Response
+ghcjsHandler mode package =
+  msum $ [ foo ] ++ map servejs ["/lib.js", "/rts.js", "/lib1.js", "/out.js" ]
+  where servejs fp = do
+          liftIO $ logM "Server.hs/ghcjsHandler" ALERT $ "Serving now " ++ (basepath package </> fp)
+          dir fp $ serveFile (asContentType "application/javascript") $ (basepath package) </> fp
+        foo = dir "/index.html" $ do liftIO $ putStrLn "Server.hs/ghcjsHandler" >> putStrLn ("Serving / with "  ++ (basepath package </> "index.html"))
+                                     serveFile (asContentType "text/html") $ basepath package </> "index.html"
+        basepath p = 
+          case mode of
+            Production -> "/usr/bin" </> (p <.> "jsexe")
+            Development -> "/home/beshers/alldarcs/src.seereason.com/happstack-ghcjs-client/dist/build/happstack-ghcjs-client" </> (p <.> "jsexe")
+
+setupLogger :: FilePath -> LogMode -> IO ()
+setupLogger logDir m = do
+    createDirectoryIfMissing True logDir
+    appLog    <- fileHandler (logDir </> "app.log")   DEBUG
+    accessLog <- fileHandler (logDir </> "access.log") INFO
+    stdoutLog <- streamHandler stdout DEBUG
+    case m of
+      Development -> do
+          -- Root Log
+          updateGlobalLogger rootLoggerName
+            (setLevel DEBUG . setHandlers [appLog, stdoutLog])
+          -- Access Log
+          updateGlobalLogger "Happstack.Server.AccessLog.Combined"
+            (setLevel DEBUG . setHandlers [accessLog])
+
+      Production -> do
+          -- Root Log
+          updateGlobalLogger rootLoggerName
+            (setLevel INFO . setHandlers [appLog])
+          -- Access Log
+          updateGlobalLogger "Happstack.Server.AccessLog.Combined"
+            (setLevel INFO . setHandlers [accessLog])
