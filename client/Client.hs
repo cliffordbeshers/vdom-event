0349bd6140e79156ac197bcf4fe63424a342cfca
"Clifford Beshers <beshers@seereason.com>
2014-07-05 02:00:59 +0000

    make the hello world account for the string bug

--- client/Client.hs
+++ client/Client.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import GHCJS.Concurrent
-import GHCJS.Types
-import GHCJS.Foreign
-import GHCJS.Marshal
-import GHCJS.DOM
+-- import GHCJS.Concurrent
+-- import GHCJS.Types
+-- import GHCJS.Foreign
+-- import GHCJS.Marshal
+-- import GHCJS.DOM
 
+import GHCJS.DOM (webViewGetDomDocument, runWebGUI)
+import GHCJS.DOM.Document (documentGetBody)
+import GHCJS.DOM.HTMLElement (htmlElementSetInnerHTML)
 
-main :: IO ()
-main = putStrLn "Hello, world!"
+f n = replicate n ' '
+
+main = runWebGUI $ \ webView -> do
+    Just doc <- webViewGetDomDocument webView
+    Just body <- documentGetBody doc
+    putStrLn "Hello, world!"
+    htmlElementSetInnerHTML body ("<h1>Hello World.</h1>")
