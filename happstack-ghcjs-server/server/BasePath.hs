aad3abe22cfb1b2ad5ca867c0e5e7d1cfdf4bccc
"Clifford Beshers <beshers@seereason.com>
2014-08-11 16:21:55 +0000

    darcs add server/BasePath.hs

---
+++ happstack-ghcjs-server/server/BasePath.hs
@@ -0,0 +1,11 @@
+module BasePath where
+
+import System.FilePath
+
+-- Separate module because template haskell can't handle this in line.
+-- But this is still wrong.  Not sure how to set this up so that
+-- external resources are embedded for production but dynamically
+-- reloaded for development.
+
+basepath :: FilePath
+basepath = "/home/beshers/alldarcs/src.seereason.com/happstack-ghcjs/happstack-ghcjs-server" </> "embedded/livedevel"
