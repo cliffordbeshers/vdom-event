0f403036efa3c840fc127833a4b426f69f8f64fc
"Clifford Beshers <beshers@seereason.com>
2014-09-11 23:11:40 +0000

    improved manifest, with checksums

--- happstack-ghcjs-server/server/Manifest.hs
+++ happstack-ghcjs-server/server/Manifest.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 -- Generate an html5 manifest file, a list of all resources
@@ -15,14 +16,15 @@ import Data.ByteString.Base16 as Base16 (encode)
 import Data.Text.Encoding as Text (decodeUtf8)
 
 import Data.Monoid ((<>))
-import Data.Text as Text
+import Data.Text as Text (Text, unlines, pack)
 import Data.Text.IO as Text (putStr)
 import Data.Time
 import Data.Time.Clock (getCurrentTime, UTCTime)
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Lift
-import "network-uri" Network.URI
+import "network" Network.URI
+import EmbedURI (embedRelativeURI)
 import WebModule
 
 default (Text)
@@ -31,34 +33,25 @@ default (Text)
 type WS = WebModule
 data WSE = WSE URI B.ByteString | WSN URI | WS_FALLBACK URI URI
 
-convert :: WebModule -> WS
+convert :: WebModule -> [WSE]
 convert (WebModule wis) = map cv wis
   where cv (WI _ uri content) = WSE uri content
 
 manifestURL :: URI
-manifestURL = "/manifest.appcache"
+manifestURL = $(embedRelativeURI "/manifest.appcache")
 
 genManifest :: WS -> Text
 genManifest = genManifestChecksum
-
-genManifest :: WS -> Text
-genManifest = genManifestChecksum
-
-genManifest' :: WS -> Text
-genManifest' ws = Text.unlines $ header ++ cache ws ++ network ws ++ fallback ws
-  where header = ["CACHE MANIFEST", "# Build Date: " <> buildDate ]
-        cache ws = "CACHE:" : [ url | WSE url content <- ws ]
-        network ws = "NETWORK:" : ["*"]
-        fallback ws = "FALLBACK:" : []
 
 genManifestChecksum :: WS -> Text
-genManifestChecksum ws = Text.unlines $ header ++ cache ws ++ network ws ++ fallback ws
-  where header = ["CACHE MANIFEST", "# Build Date: " <> buildDate ]
+genManifestChecksum ws' = Text.unlines $ header ++ cache ws ++ network ws ++ fallback ws
+  where header = ["CACHE MANIFEST"] -- , "# Build Date: " <> buildDate ]
         cache ws = "CACHE:" : [ showURL url <> " # " <> checksum content | w@(WSE url content) <- ws ]
         network ws = "NETWORK:" : ["*"]
         fallback ws = "FALLBACK:" : []
-        showURL = id
+        showURL = Text.pack . show
         checksum = Text.decodeUtf8 . Base16.encode . MD5.hash
+        ws = convert ws'
 
 toStrict1 :: BL.ByteString -> B.ByteString
 toStrict1 = B.concat . BL.toChunks
