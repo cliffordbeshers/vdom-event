449ea964c53fc5e29c2b61d4c0a2a7fb8748802f
"Clifford Beshers <beshers@seereason.com>
2014-10-11 05:51:17 +0000

    Clean up the placement of binary build files in local-development.sh

--- local-development.sh
+++ local-development.sh
@@ -7,13 +7,13 @@ set -x
 
 ( cd happstack-ghcjs-client
   rm -rf dist
-  runhaskell Setup.hs --ghcjs configure
+  runhaskell Setup.hs --ghcjs configure --ghcjs-option="-odir dist/build/tmp" --ghcjs-option="-hidir dist/build/tmp"
   runhaskell Setup.hs build
 )
 
 ( cd happstack-ghcjs-server
   runhaskell Setup.hs configure -f local-development
-  runhaskell Setup.hs build
+  runhaskell Setup.hs build --ghc-option="-odir dist/build/tmp" --ghc-option="-hidir dist/build/tmp"
   dist/build/happstack-ghcjs-server/happstack-ghcjs-server
   # runhaskell Setup.hs run
 )
