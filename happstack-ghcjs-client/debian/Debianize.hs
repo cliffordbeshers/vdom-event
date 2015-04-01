19b623825acbdfbd9742e72a1189919f42e9098b
David Fox <dsf@seereason.com>
2015-04-01 11:37:48 -0700

    Fix the debian build

--- happstack-ghcjs-client/debian/Debianize.hs
+++ happstack-ghcjs-client/debian/Debianize.hs
@@ -1,15 +1,23 @@
+import Control.Applicative ((<$>))
 import Control.Lens hiding ((+=))
 import Debian.Debianize
 import Debian.AutoBuilder.Details.Versions (seereasonDefaults)
 import Debian.Relation (BinPkgName(BinPkgName))
+import Distribution.Compiler (CompilerFlavor(GHCJS))
+import System.FilePath ((</>))
+import System.FilePath.Find
 
 main :: IO ()
-main =
-    newFlags >>= newCabalInfo >>= evalCabalT (debianize (seereasonDefaults >> customize) >> liftCabal writeDebianization)
+main = do
+  hsFiles <- concat <$> (mapM (find always (extension ==? ".hs")) ["client", "GHCJSStub"])
+  newFlags >>= newCabalInfo >>= evalCabalT (debianize (seereasonDefaults >> customize hsFiles) >> liftCabal writeDebianization)
     where
-      customize =
+      customize hsFiles =
           do (debInfo . utilsPackageNameBase) ~= Just "happstack-ghcjs-client"
              (debInfo . sourceFormat) ~= Just Native3
-             (debInfo . atomSet) += InstallTo (BinPkgName "happstack-ghcjs-client")
-                                              "client/Common.hs"
-                                              "usr/share/happstack-ghcjs/Common.hs"
+             (debInfo . flags . compilerFlavor) ~= GHCJS
+             -- cabal-debian currently can't correctly install the
+             -- names listed in data-files, so list these explicitly.
+             mapM_ dataFile hsFiles
+
+      dataFile path = (debInfo . atomSet) += InstallTo (BinPkgName "happstack-ghcjs-client") path ("usr/share/happstack-ghcjs-client" </> path)
