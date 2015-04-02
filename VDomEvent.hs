35030950d9dd84b7292cebc65973cc7d9649a6cd
Clifford Beshers <cliffordbeshers@gmail.com>
2015-04-02 01:12:04 -0500

    RWS monad seems to be working, but hard to test because the event handler is now busted.  Next step.

--- VDomEvent.hs
+++ VDomEvent.hs
@@ -2,7 +2,7 @@
 
 import Control.Concurrent
 import Control.Monad
-import Control.Monad.Writer
+import Control.Monad.RWS.Lazy
 import Data.Default
 import Data.Monoid
 import Data.Text (Text, pack)
@@ -16,6 +16,11 @@ import Bootstrap
 import UtilQQ
 
 default (Text)
+
+type MkHandler = IO (IO ())
+
+type Application state = RWS (MVar state) [MkHandler] state VNode
+type ApplicationT m state = RWST (MVar state) [MkHandler] state m VNode
 
 update :: MVar State -> IO State
 update state = do
@@ -33,46 +38,53 @@ inlineBlock = cls "ib"
 loadInlineBlock :: Text -> IO ()
 loadInlineBlock tag = void $ inlineCSS (tag <> " { display: inline-block; }")
 
-render :: State -> Writer [IO (IO ())] VNode
-render (State i) = do tell $ [print "attach" >> return (print "detach") ]
-                      dom i
-  where dom i = if i < 1 then return $ button_ noProps $$ [textt (textshow i)]
-                else do left <- render (State (i-1))
-                        right <- render (State (i-1))
-                        return $ div_ inlineBlock $$ [ button_ noProps $$ [textt (textshow i)], left, right ]
+render :: Application State
+render = do
+  State i <- get
+  tell $ [print "attacher" >> return (print "detacher") ]
+  dom i
+    where dom i = if i < 1 then return $ button_ noProps $$ [textt (textshow i)]
+                  else do left <- recurse render
+                          right <- recurse render
+                          return $ div_ inlineBlock $$ [ button_ noProps $$ [textt (textshow i)], left, right ]
+          recurse :: Application State -> Application State
+          recurse = withRWS (\r s -> (r, succ' s))
+          succ' (State i) = State $ succ i
+          
+
+-- Add a Value Supply monad.
+-- Attach an id to each node, or perhaps to each click target.
+-- run click byId  
 
 main = do
+  print "No button"
   b <- select "<button>Click me.</button>"
   select "body" >>= appendJQuery b
-  state <- newMVar (State 0)
-  redraw <- newEmptyMVar
+--   click (\e -> do { m <- fmap mkMsg (update  state); print ("click",m) ;  putMVar redraw m }) def b
 
-  click (\e -> do { m <- fmap mkMsg (update  state); print ("click",m) ;  putMVar redraw m }) def b
-
-  eventLoop render redraw
+  eventLoop render (State 0)
 
   return ()
 
 
 -- The outer IO creates an event handler, the returned IO cleans it up
-type MkHandler = IO (IO ())
-
-eventLoop :: (State -> Writer [MkHandler] VNode) -> MVar State -> IO ThreadId
-eventLoop application redrawChannel =
+eventLoop :: Application State -> State -> IO ThreadId
+eventLoop application s0 =
   forkIO $ do
       top <- mkRoot
       loadInlineBlock "div"
       loadBootstrap
       lastdraw <- newMVar (emptyDiv, [])
+      redrawChannel <- newMVar s0
       forever $ do
-        msg <- takeMVar redrawChannel
-        print ("fork",msg)
-        (r,detach) <- takeMVar lastdraw
-        let (r', attach') = runWriter $ render msg
-        let p' = diff r r'
-        sequence detach
+        state <- takeMVar redrawChannel
+        print ("fork",state)
+        (r0,detachers0) <- takeMVar lastdraw
+        let (r1, s1, attachers1) = runRWS application redrawChannel state
+        let p' = diff r0 r1
+        sequence detachers0
         patch top p'
-        detach' <- sequence attach'
-        putMVar lastdraw (r', detach')
+        detachers1 <- sequence attachers1
+        putMVar lastdraw (r1, detachers1)
         
         
