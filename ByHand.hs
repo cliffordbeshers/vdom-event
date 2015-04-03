3200480c2a2cda6c6f734bff04ad712e02773d0d
Clifford Beshers <cliffordbeshers@gmail.com>
2015-04-03 16:05:07 -0500

    Printfs that show the event detachers are not going away.

--- ByHand.hs
+++ ByHand.hs
@@ -21,21 +21,6 @@ default (Text)
 
 type MkHandler = IO (IO ())
 
-onesec :: IO ()
-onesec = threadDelay 1000000
-
--- type Application state = RWST (MVar (state -> state)) [MkHandler] state (Supply Integer) VNode
-type ApplicationT m state = RWST (MVar (state -> state)) [MkHandler] state (SupplyT Integer m)  VNode
-type Application state = ApplicationT IO state
-
--- update :: MVar State -> IO State
--- update state = do
---   State i <- takeMVar state
---   putMVar state (State (i+1))
---   return $ State (i+1)
-
-mkMsg = id
-
 data State = State Int deriving (Show)
 
 inlineBlock :: Properties
@@ -48,10 +33,12 @@ loadInlineBlock tag = void $ inlineCSS (tag <> " { display: inline-block; }")
 click' :: (MVar (State-> State)) -> Text -> (State -> State) -> IO (IO ())
 click' redrawChannel ident update = do
   select (byId ident) >>= click hnd def
-    where hnd _e = putMVar redrawChannel update
+    where hnd e = do
+            print ("hnd", ident)
+            putMVar redrawChannel update
 
 succ' :: State -> State
-succ' (State i) = State (succ i)
+succ' (State i) = State (1+i)
 
 render :: MVar (State -> State) -> State -> IO (VNode, IO (IO ()))
 render redrawChannel (State i) = do
@@ -73,6 +60,7 @@ eventLoop :: DOMNode -> (MVar (State -> State) -> State -> IO (VNode, IO (IO ())
 eventLoop root render state redrawChannel (olddom, olddetacher) = do
   update <- takeMVar redrawChannel
   let newstate = update state
+  print ("newstate",newstate)
   (newdom, newattacher) <- render redrawChannel newstate
   let p' = diff olddom newdom
   olddetacher
