def5c12e7d083a123520cb90cc7d151ef5c5fe9f
Clifford Beshers <cliffordbeshers@gmail.com>
2015-04-29 10:30:44 -0500

    Copying in Alder.Html files by hand.

---
+++ Alderon/Html.hs
@@ -0,0 +1,14 @@
+module Alderon.Html
+    ( Html
+    , text
+    , Handler(..)
+    , Attribute
+    , Attributable
+    , (!)
+    , (!?)
+    , (!#)
+    , (!.)
+    , (!?.)
+    ) where
+
+import           Alderon.Html.Internal
