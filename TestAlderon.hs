19de891ae7e248b854bdf3d77e4e0ca51437ac64
Clifford Beshers <cliffordbeshers@gmail.com>
2015-05-28 02:00:28 -0500

    Simple image examples.

--- TestAlderon.hs
+++ TestAlderon.hs
@@ -37,6 +37,22 @@ instance Monoid W where
 
 type Z a = RWST R W S a
 
+
+type URL = Text
+
+imageURLs :: [URL]
+imageURLs =
+  [ "https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/11082361_10204865703177218_7142048938861798417_o.jpg"
+  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/s720x720/10676351_10204875971793927_291876182821710665_n.jpg?oh=77cf176c31c46ac933272de0480dcfe1&oe=55818649"
+  , "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpf1/t31.0-8/10914721_10204856412024945_2607390891971097151_o.jpg"
+  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10988282_10204849112762468_7979569982779097370_n.jpg?oh=712dc71ac4a1a5c7a6d452ec57d6799f&oe=5583185E"
+  , "https://scontent-ord.xx.fbcdn.net/hphotos-xfp1/t31.0-8/11073500_10204842190469415_875697331045021877_o.jpg"
+  , "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10710956_10204829354548525_3840378263239480270_n.jpg?oh=a6f915cf68b916c6c84c6e8baeecb028&oe=55B52B34&__gda__=1434306594_f075e1695ab5a3a56ff0a6d7921b6254"
+  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10983182_10204802735643069_2375287011727111812_n.jpg?oh=c57d524c6be33146eb175548a7f46200&oe=5573FFE1"
+  , "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-prn2/v/t1.0-9/11056894_10204795437740626_1302633900976191151_n.jpg?oh=da776a52e13eba9c1426b03708f1fd15&oe=557A2D8C&__gda__=1437981455_ed50f8cf7a166212a9e087b81fb3cd5c"
+  ]
+
+
 hello :: MVar (Bool -> Bool) -> Bool -> Html
 hello  redrawChannel b = let inputText = "Hello this is in put" in
   div_ !# (if b then "Hello Header" else "Goodbye Header") $ do
@@ -45,6 +61,7 @@ hello  redrawChannel b = let inputText = "Hello this is in put" in
       textarea b bigText ! onInput' >> br_
       mkCheckbox "Check1" "Check2" >> (text_ "Check") >> br_
       mkSelect "menu1" [("1","One"),("2","Two"),("3","Three")] "3" >>  br_
+      mapM_ (\u -> mkImg u 50 50 >> br_) imageURLs
       
   where clicker = onClick (Inputt (\e -> print "clicker" >> putMVar redrawChannel not))
         onInput' = onInput (Inputt (\e -> print "input" >> putMVar redrawChannel id))
@@ -81,6 +98,10 @@ textarea b t = form' (xform b t)
 
 mkButton :: Html -> Html
 mkButton = button_ ! type_ "button"
+
+mkImg :: URL -> Int -> Int -> Html
+mkImg url w h =
+  img_ ! src_ url ! width_ (tshow w) ! height_ (tshow h)
 
 mkKeygen :: Text -> Html
 mkKeygen nm = keygen_ ! name_ nm
