5a8bee2e85ad51a5176e66fdf72897c43618887c
David Fox <dsf@seereason.com>
2015-06-10 15:52:38 -0700

    further update debiainization scripts

--- happstack-ghcjs-webmodule/debian/Debianize.hs
+++ happstack-ghcjs-webmodule/debian/Debianize.hs
@@ -1,19 +1,20 @@
 import Control.Monad.State (get)
 import Control.Lens
 import Debian.Debianize
+import Debian.Debianize.Optparse (parseProgramArguments, CommandLineOptions(..))
 import Distribution.Compiler
 import Debian.Relation
 
 main :: IO ()
-main = newFlags >>= newCabalInfo >>= evalCabalT (debianize (debianDefaults >> customize) >> liftCabal writeDebianization)
+main = parseProgramArguments >>= newCabalInfo . _flags >>= evalCabalT (debianize (debianDefaults >> customize) >> liftCabal writeDebianization)
 
 customize :: CabalT IO ()
 customize = do
   hc <- get >>= return . view (debInfo . flags . compilerFlavor)
   case hc of
-    GHC -> (debInfo . sourceFormat) .= Just Native3
+    GHC -> (debInfo . sourceFormat) .= Native3
     GHCJS ->
         do let (Right rels) = parseRelations "ghcjs, libghc-cabal-122-dev, haskell-devscripts (>= 0.8.21.3)"
            (debInfo . sourcePackageName) .= Just (SrcPkgName "ghcjs-happstack-ghcjs-webmodule")
-           (debInfo . sourceFormat) .= Just Native3
+           (debInfo . sourceFormat) .= Native3
            (debInfo . control . buildDepends) %= (++ rels)
