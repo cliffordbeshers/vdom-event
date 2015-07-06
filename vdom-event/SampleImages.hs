44b23409536f94be2aed7b9f03d0484877285a6f
Clifford Beshers <cliffordbeshers@gmail.com>
2015-07-06 16:51:32 -0500

    New images with stable urls in SampleImages.

--- vdom-event/SampleImages.hs
+++ vdom-event/SampleImages.hs
@@ -8,13 +8,15 @@ default (Text)
 
 imageURLs :: [Text]
 imageURLs =
-  [ "https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/11082361_10204865703177218_7142048938861798417_o.jpg"
-  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/s720x720/10676351_10204875971793927_291876182821710665_n.jpg?oh=77cf176c31c46ac933272de0480dcfe1&oe=55818649"
-  , "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpf1/t31.0-8/10914721_10204856412024945_2607390891971097151_o.jpg"
-  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10988282_10204849112762468_7979569982779097370_n.jpg?oh=712dc71ac4a1a5c7a6d452ec57d6799f&oe=5583185E"
-  , "https://scontent-ord.xx.fbcdn.net/hphotos-xfp1/t31.0-8/11073500_10204842190469415_875697331045021877_o.jpg"
-  , "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10710956_10204829354548525_3840378263239480270_n.jpg?oh=a6f915cf68b916c6c84c6e8baeecb028&oe=55B52B34&__gda__=1434306594_f075e1695ab5a3a56ff0a6d7921b6254"
-  , "https://scontent-ord.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10983182_10204802735643069_2375287011727111812_n.jpg?oh=c57d524c6be33146eb175548a7f46200&oe=5573FFE1"
-  , "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-prn2/v/t1.0-9/11056894_10204795437740626_1302633900976191151_n.jpg?oh=da776a52e13eba9c1426b03708f1fd15&oe=557A2D8C&__gda__=1437981455_ed50f8cf7a166212a9e087b81fb3cd5c"
+  [ "https://www.facebook.com/photo.php?fbid=10205727901251631&l=18589f7f04"
+  , "https://www.facebook.com/photo.php?fbid=10205694236330029&l=e54c2ca1df"
+  , "https://www.facebook.com/photo.php?fbid=10205736881796139&l=2e00038ff0"
+  , "https://www.facebook.com/photo.php?fbid=10205660064795762&l=72d51726f6"
+  , "https://www.facebook.com/photo.php?fbid=10205656505746788&l=de71a7d405"
+  , "https://www.facebook.com/photo.php?fbid=10205596917177111&l=a1564b91e4"
+  , "https://www.facebook.com/photo.php?fbid=10205532857615662&l=423822b3d9"
+  , "https://www.facebook.com/photo.php?fbid=10205499530862514&l=2eed5d0cdd"
+  , "https://www.facebook.com/photo.php?fbid=10205469578553725&l=426cd83ae8"
   ]
+
 
